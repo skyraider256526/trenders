574d141dc9e48fe55175872b917a50497dae54ce
skyraider <akashyadav@gmail.com>
2019-09-27 08:44:14 +0530

    updated shop data, added cart utils to group items

--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import "./custom-button.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
   <button
-    className={`custom-button ${isGoogleSignIn ? "google-sign-in" : ""}`}
+    className={`custom-button ${inverted ? "inverted" : ""} ${isGoogleSignIn ? "google-sign-in" : ""}`}
     {...otherProps}
   >
     {children}
