d0d2a0a36df50185f43600271ddd8699dce9ed11
skyraider <akashyadav@gmail.com>
2019-11-03 17:28:59 +0530

    added gh-pages

--- src/pages/shoppage/shop.jsx
+++ src/pages/shoppage/shop.jsx
@@ -1,27 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-import SHOP_DATA from "./shopdata";
-import CollectionPreview from "../../components/collection-preview/collection-preview";
+import { Route } from "react-router-dom";
 
-class ShopPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-  render() {
-    return (
-      <div className="shop">
-        {this.state.collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview
-            key={id}
-            {...otherCollectionProps}
-          ></CollectionPreview>
-        ))}
-      </div>
-    );
-  }
-}
+import CollectionPage from "../collection/collection.page";
+
+import CollectionsOverview from "../../components/collection-overview/collection-overview";
+
+const ShopPage = ({ match }) => (
+  <div className="shop">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
