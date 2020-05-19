f2d46c91e49c457436379c3e4c1a90fec0c96e95
skyraider <akashyadav@gmail.com>
2020-05-19 17:21:21 +0530

    Updates

---
+++ precache-manifest.fa4171eece22ecccf88acda342384ba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b182e5a3a9e53e8aeac039de2893bcb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed46148639fb82a46e23",
+    "url": "/static/css/main.db21947e.chunk.css"
+  },
+  {
+    "revision": "de5e3434fbb3e80cdb61",
+    "url": "/static/js/2.96ae9651.chunk.js"
+  },
+  {
+    "revision": "ed46148639fb82a46e23",
+    "url": "/static/js/main.c8113ff1.chunk.js"
+  },
+  {
+    "revision": "dc6e99c1d62f428f7e39",
+    "url": "/static/js/runtime-main.4aab2101.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
